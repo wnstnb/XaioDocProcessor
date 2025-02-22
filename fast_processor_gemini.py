39f5a77078d38b55c79e279689850133baebc0f8
Winston Bandong <w.bandong@gmail.com>
2025-02-22 14:42:21 -0800

    one more time

--- fast_processor_gemini.py
+++ fast_processor_gemini.py
@@ -22,7 +22,6 @@ from google import genai
 from pydantic import BaseModel, Field
 from gemini_models import get_model, extract_structured_data
 from tenacity import retry, wait_random_exponential
-from entity_matcher import match_entities_for_file
 
 
 
@@ -511,7 +510,6 @@ def process_file(fp):
             res['page_num'] = row['page_number']
             extraction_results.append(res)
             info_results.append(info)
-            match_entities_for_file(os.path.basename(fp))
 
     df_pages['clf_type'] = clf_types
     df_pages['page_label'] = clf_results
